81ff7b2ba57d231f79378615393435cdbe429ac7
imarkonis <you@example.comimarkonis@gmail.com>
2025-05-12 06:38:50 +0000

    memory in ExEvEs

--- projects/exeves_memory/00_initialize.R
+++ projects/exeves_memory/00_initialize.R
@@ -1,12 +1,16 @@
 #Creates project paths
 
 source("source/main.R")
+source("source/exeves.R")
+source("database/06_dataset_fnames.R")
 
-PATH_OUTPUT <- paste0(PATH_SAVE, "exeves_memory/")
+## Paths
+PATH_OUTPUT <- paste0(PATH_SAVE, "exeves/")
 PATH_OUTPUT_DATA <- paste0(PATH_OUTPUT, "data/")
 PATH_OUTPUT_RAW <- paste0(PATH_OUTPUT_DATA, "raw/")
 PATH_OUTPUT_FIGURES <- paste0(PATH_OUTPUT, "figures/")
 PATH_OUTPUT_TABLES <- paste0(PATH_OUTPUT, "tables/")
+PATH_INPUT_RAW <- "../../shared/data/stations/evap/eddy_zao/"
 
 dir.create(PATH_OUTPUT, showWarnings = FALSE)
 dir.create(PATH_OUTPUT_DATA, showWarnings = FALSE)
@@ -19,4 +23,5 @@ save(PATH_OUTPUT,
      PATH_OUTPUT_DATA,
      PATH_OUTPUT_FIGURES,
      PATH_OUTPUT_TABLES,
+     PATH_INPUT_RAW,
      file = paste0(PATH_OUTPUT, "paths.Rdata"))

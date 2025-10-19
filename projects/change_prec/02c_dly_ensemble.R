7f9a2c3ffd1f1d31f63128f8511d8a2973e867c3
MiRoVaGo <mirovago@gmail.com>
2025-10-19 20:03:45 +0000

    add dataset name to rNx data

--- projects/change_prec/02c_dly_ensemble.R
+++ projects/change_prec/02c_dly_ensemble.R
@@ -5,8 +5,18 @@ source("source/change_prec.R")
 load(paste0(PATH_SAVE_CHANGE_PREC, "prec_names_1991_2024.rda"))
 
 ## Analysis
-foreach(data_count = 1:length(PREC_NAMES_1991_2024_DLY)) %do% {
-  dummie <- tabular(PREC_NAMES_1991_2024_DLY[data_count])
+foreach(year_count = 1991:2024) %do% {
+  dummie <- grep(year_count, PREC_NAMES_1991_2024_DLY, value = TRUE)
+  dummie <- lapply(dummie, function(x) {
+    dummie_name <- sub(".*/([^_/]*)_.*", "\\1", x)
+    x <- tabular(x)
+    x$dataset <- dummie_name
+    x$date <- as.Date(x$date)
+    x
+  })
+  dummie <- rbindlist(dummie)
+  dummie <- dummie[, .(value = median(value, na.rm = TRUE)),
+                   .(lon, lat, date)]
   dummie[, `:=`(s3x = frollsum(value, 3, na.rm = TRUE, align = "left"),
                 s5x = frollsum(value, 5, na.rm = TRUE, align = "left")),
          .(lon, lat)]
@@ -15,10 +25,9 @@ foreach(data_count = 1:length(PREC_NAMES_1991_2024_DLY)) %do% {
                 r5x = max(s5x, na.rm = TRUE)),
          .(lon, lat, year(date))]
   dummie <- dummie[r1x == value | r3x == s3x | r5x == s5x]
-  dummie_name <- sub(".*/([^_/]*)_.*", "\\1", PREC_NAMES_1991_2024_DLY[data_count])
-  dummie$dataset <- dummie_name
+  dummie$dataset <- "ensemble"
   saveRDS(dummie,
-          paste0(PATH_SAVE_CHANGE_PREC_TEMP, "temp_", data_count,".rds"))
+          paste0(PATH_SAVE_CHANGE_PREC_TEMP, "temp_", year_count,".rds"))
   rm(dummie)
   gc()
 }
@@ -32,12 +41,17 @@ dummie <- lapply(temp_filelist, readRDS)
 dummie <- lapply(dummie, function(x) {
   x <- x[r1x == value, .(lon, lat, date = as.Date(date), dataset, prec = r1x)]
   x
-  })
+})
 dummie <- rbindlist(dummie)
+
+dummie_base <- readRDS(paste0(PATH_SAVE_CHANGE_PREC,
+                              "prec_data_prec_data_r1x.rds"))
+
+dummie <- rbind(dummie_base, dummie)
 
 saveRDS(dummie,
         paste0(PATH_SAVE_CHANGE_PREC, "prec_data_prec_data_r1x.rds"))
-rm(dummie)
+rm(dummie, dummie_base)
 gc()
 
 dummie <- lapply(temp_filelist, readRDS)
@@ -47,9 +61,14 @@ dummie <- lapply(dummie, function(x) {
 })
 dummie <- rbindlist(dummie)
 
+dummie_base <- readRDS(paste0(PATH_SAVE_CHANGE_PREC,
+                              "prec_data_prec_data_r3x.rds"))
+
+dummie <- rbind(dummie_base, dummie)
+
 saveRDS(dummie,
         paste0(PATH_SAVE_CHANGE_PREC, "prec_data_prec_data_r3x.rds"))
-rm(dummie)
+rm(dummie, dummie_base)
 gc()
 
 dummie <- lapply(temp_filelist, readRDS)
@@ -59,9 +78,14 @@ dummie <- lapply(dummie, function(x) {
 })
 dummie <- rbindlist(dummie)
 
+dummie_base <- readRDS(paste0(PATH_SAVE_CHANGE_PREC,
+                              "prec_data_prec_data_r5x.rds"))
+
+dummie <- rbind(dummie_base, dummie)
+
 saveRDS(dummie,
         paste0(PATH_SAVE_CHANGE_PREC, "prec_data_prec_data_r5x.rds"))
-rm(dummie)
+rm(dummie, dummie_base)
 gc()
 
 ## Clear temp files
---
+++ projects/change_prec/09c_r5x_slopes.r
@@ -0,0 +1,82 @@
+#Scatter plot matrix
+source("source/change_prec.R")
+
+registerDoParallel(cores = N_CORES)
+
+## Data
+done_ids <- list.files(PATH_SAVE_CHANGE_PREC_TEMP, full.names = TRUE)
+done_ids <- sub(".*/([^_]+)_.*", "\\1", done_ids) %>% as.numeric() %>% unique()
+gc()
+
+prec_data <- readRDS(paste0(PATH_SAVE_CHANGE_PREC,
+                            "prec_data_prec_data_r5x.rds"))
+gc()
+
+## Analysis
+prec_data[, coord_id := .GRP, by = c("lon", "lat")]
+
+COORD_MAX <- max(prec_data$coord_id)
+
+dummie_coords <- 1:COORD_MAX
+dummie_coords <- setdiff(dummie_coords, done_ids)
+gc()
+
+prec_data <- prec_data[coord_id %in% dummie_coords]
+gc()
+
+DATASETS <- c("cpc-global", "ensemble", "era5-land", "gpcp-v1-3",
+              "gpm-imerg-v7", "jra-3q", "merra-2", "mswep-v2-8", "ncep-doe")
+
+foreach (coord_idx = 1:length(dummie_coords)) %dopar% {
+  idx <- dummie_coords[coord_idx]
+  dummie_point <- prec_data[coord_id == idx]
+  dummie <- foreach(data_idx = 1:9, .combine = rbind) %do% {
+    dummie_dataset <- DATASETS[data_idx]
+    dummie_row <- dummie_point[dataset == dummie_dataset]
+    if (nrow(dummie_row) > 1) {
+      dummie_1990_2019 <- dummie_row[year(date) <= 2019]
+      dummie_1995_2024 <- dummie_row[year(date) >= 1995]
+      dummie_time_1990_2019 <- 1:nrow(dummie_1990_2019)
+      dummie_time_1995_2024 <- 1:nrow(dummie_1995_2024)
+      X_1990_2019 <- cbind(1, dummie_time_1990_2019)
+      X_1995_2024 <- cbind(1, dummie_time_1995_2024)
+      invXtX_1990_2019 <- solve(t(X_1990_2019) %*% X_1990_2019) %*% t(X_1990_2019)
+      invXtX_1995_2024 <- solve(t(X_1995_2024) %*% X_1995_2024) %*% t(X_1995_2024)
+      dummie_slope_1990_2019 <- (invXtX_1990_2019  %*% dummie_1990_2019$prec)[2]
+      dummie_slope_1995_2024 <- (invXtX_1995_2024 %*% dummie_1995_2024$prec)[2]
+      dummie_row <- unique(dummie_row[, .(lon, lat, dataset)])
+      dummie_row$slope_1990_2019 <- dummie_slope_1990_2019
+      dummie_row$slope_1995_2024 <- dummie_slope_1995_2024
+      dummie_row$max_1990_2019 <- max(dummie_1990_2019$prec, na.rm = TRUE)
+      dummie_row$max_1995_2024 <- max(dummie_1995_2024$prec, na.rm = TRUE)
+      dummie_row$min_1990_2019 <- min(dummie_1990_2019$prec, na.rm = TRUE)
+      dummie_row$min_1995_2024 <- min(dummie_1995_2024$prec, na.rm = TRUE)
+      dummie_row$iqr_1990_2019 <- IQR(dummie_1990_2019$prec, na.rm = TRUE)
+      dummie_row$iqr_1995_2024 <- IQR(dummie_1995_2024$prec, na.rm = TRUE)
+      dummie_row$median_1990_2019 <- median(dummie_1990_2019$prec, na.rm = TRUE)
+      dummie_row$median_1995_2024 <- median(dummie_1995_2024$prec, na.rm = TRUE)
+      dummie_row$mean_1990_2019 <- mean(dummie_1990_2019$prec, na.rm = TRUE)
+      dummie_row$mean_1995_2024 <- mean(dummie_1995_2024$prec, na.rm = TRUE)
+      return(dummie_row)
+    }
+  }
+  fwrite(dummie, paste0(PATH_SAVE_CHANGE_PREC_TEMP, idx, "_tmp.csv"))
+  rm(dummie, dummie_point)
+  gc()
+}
+
+## Save
+temp_filelist <- list.files(PATH_SAVE_CHANGE_PREC_TEMP, full.names = TRUE,
+                            pattern = "*_tmp.csv")
+
+if (length(temp_filelist) == COORD_MAX) {
+  prec_data <- lapply(temp_filelist, fread)
+  prec_data <- rbindlist(prec_data)
+  
+  ## Save data
+  saveRDS(prec_data, file = paste0(PATH_SAVE_CHANGE_PREC,
+                                   "prec_data_r5x_slopes.rds"))
+  
+  ## Clear temporary files
+  file.remove(temp_filelist)
+}
